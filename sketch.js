834f4374612d9544f43aceaa4723de6996db97a3
the-Fish2 <97804639+the-Fish2@users.noreply.github.com>
2022-07-28 12:47:38 -0700

    Kinda wallpaper pattern? reflection, not rotation

--- sketch.js
+++ sketch.js
@@ -3,25 +3,35 @@
 //first simulate the reflection, glide reflection, translation, rotation code?
 //maybe instead matrix multiplication?? to get new points
 //that should be done in python
-const canva_size = 400
-const lattice = 0
+const canva_size = 750
+const lattice = 2
 const num = 1
 const groupNames = ["p1", "p2", "pm", "pg", "cm", "pmm", "pmg", "pgg", "cmm", "p4", "p4m", "p4g", "p3", "p3m1", "p31m", "p6", "p6m"]
+var base = []
 
 function setup() {
   // put setup code here
   createCanvas(canva_size, canva_size);
   background(250);
    
-  const points = latticeBase()
+  base = latticeBase()
+  const points = grid(base[0], base[1])
   isometries(points)
 }
 
 function draw() {
   if (mouseIsPressed) {
     fill(0)
-    point(mouseX, mouseY)
-    isometries([[mouseX, mouseY]])
+    // point(mouseX, mouseY)
+    if (num == 0) {
+      wallpaper0()
+    }
+    else if (num == 1) {
+      let v0 = base[1].copy()
+      v0.sub(base[0]) 
+      print(v0)
+      wallpaper1(v0)
+    }
   }
 }
 
@@ -55,13 +65,13 @@ function latticeBase() {
     
   }
 
-  return grid(i, j)
+  return [i, j]
 }
 
 function grid(i, j) {
   stroke('purple')
   strokeWeight(3)
-  let i2 = createVector(-170, 170)
+  let i2 = createVector(i.x, i.y)
   let j2 = createVector(-1 * j.x, -1 * j.y)
 
   let points = [[]]
@@ -90,22 +100,97 @@ function grid(i, j) {
 function isometries(vecs = []) {
   //num = 0 - do nothing
   if (num == 1) {
-    console.log("hi")
     let v0 = createVector(-40, 50)
     for (let i = 0; i < vecs.length; i++) {
       // let a = 180
       // let changeV = createVector(vecs[i][0] * sin(a) * cos(a), )
       // triangle(changeV.x, changeV.y, changeV.x - 1, changeV.y, changeV.x, changeV.y + 1)
       let changeV = createVector(vecs[i][0], vecs[i][1])
+      //vecs[i][0] - 50 makes something diff :)
       changeV.reflect(v0) 
       //point(changeV.x, changeV.y)
       triangle(changeV.x-4, changeV.y+4, changeV.x - 5, changeV.y+4, changeV.x-4, changeV.y + 5)
     }
   }
+}
 
-  if (num == 2) {
-
+function wallpaper0() {
+  //n = 0
+ 
+  let mouseP = [pmouseX, pmouseY]
+  point(pmouseX, pmouseY)
+  while (mouseP[0] < canva_size) {
+    while (mouseP[1] < canva_size) {
+      mouseP[1] += 50
+      point(mouseP[0], mouseP[1])
+      
+    }
+    mouseP[1] = pmouseY
+    while (mouseP[1] >= 0) {
+      mouseP[1] -= 50
+      point(mouseP[0], mouseP[1])
+    }
+    mouseP[0] += 50
   }
+  mouseP[0] = pmouseX
+  while (mouseP[0] > 0) {
+    while (mouseP[1] < canva_size) {
+      mouseP[1] += 50
+      point(mouseP[0], mouseP[1])
+      
+    }
+    mouseP[1] = pmouseY
+    while (mouseP[1] >= 0) {
+      mouseP[1] -= 50
+      point(mouseP[0], mouseP[1])
+    }
+    mouseP[0] -= 50
+  }
+
+}
+
+
+function wallpaper1(v0) {
+  //n = 1
+  //should actually be i j diff
+  //let v0 = createVector(-40, 50)
+  let mouseP = [pmouseX, pmouseY]
+  point(pmouseX, pmouseY)
+  while (mouseP[0] < canva_size) {
+    while (mouseP[1] < canva_size) {
+      mouseP[1] += 50
+      isometries2([mouseP[0], mouseP[1]], v0)
+      
+    }
+    mouseP[1] = pmouseY
+    while (mouseP[1] >= 0) {
+      mouseP[1] -= 50
+      isometries2([mouseP[0], mouseP[1]], v0)
+    }
+    mouseP[0] += 50
+  }
+  mouseP[0] = pmouseX
+  while (mouseP[0] > 0) {
+    while (mouseP[1] < canva_size) {
+      mouseP[1] += 50
+      isometries2([mouseP[0], mouseP[1]], v0)
+      
+    }
+    mouseP[1] = pmouseY
+    while (mouseP[1] >= 0) {
+      mouseP[1] -= 50
+      isometries2([mouseP[0], mouseP[1]], v0)
+    }
+    mouseP[0] -= 50
+  }
+
+}
+
+function isometries2(vecs = [], v0) {
+    let changeV = createVector(vecs[0], vecs[1])
+    point(changeV.x, changeV.y)
+    changeV.reflect(v0)
+    point(changeV.x, changeV.y)
 }
 
 // function translate(x=createVector(0,0), a=createVector(50, 0)) {
