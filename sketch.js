2b20e4c91c7275a968dbfe316cd2fd5f94277f82
the-Fish2 <97804639+the-Fish2@users.noreply.github.com>
2022-07-29 17:12:53 -0700

    12/17 done

--- sketch.js
+++ sketch.js
@@ -1,6 +1,6 @@
 const canva_size = 500
 const lat_size = 100
-var pattern = 10
+var pattern = 11
 var vectors = []
 
 //make faster by cutting points already drawn
@@ -51,22 +51,17 @@ function patterned(vec) {
         //cm
         points.push(createVector(changeV.x - lat_size/2, changeV.y - lat_size/2))
         changeV.x = (lat_size/2) - changeV.x
-        points.push(createVector(changeV.x, changeV.y))
+        pushIn(points, changeV)
         points.push(createVector(changeV.x - lat_size/2, changeV.y - lat_size/2))
     }
     else if (pattern == 5) {
-        //pmm
-        changeV.reflect(v1)
-        points.push(createVector(changeV.x, changeV.y))
-        changeV.reflect(v2)
-        points.push(createVector(changeV.x, changeV.y))
-        changeV2.reflect(v2)
-        points.push(changeV2)
+        //pmm      
+        quads(v1, v2, points, changeV)
     }
     else if (pattern == 6) {
         //pmg
         changeV.x = (lat_size/2) - changeV.x
-        points.push(createVector(changeV.x, changeV.y))
+        pushIn(points, changeV)
         changeV.reflect(v2)
         points.push(createVector(changeV.x+lat_size/2, changeV.y))
         changeV2.reflect(v2)
@@ -76,7 +71,7 @@ function patterned(vec) {
         //pgg
         changeV.x = (lat_size/2) - changeV.x
         changeV.y = (lat_size/2) - changeV.y
-        points.push(createVector(changeV.x, changeV.y))
+        pushIn(points, changeV)
         changeV.reflect(v2)
         changeV.x += lat_size/2
         changeV2.reflect(v2)
@@ -88,55 +83,52 @@ function patterned(vec) {
         //cmm
         changeV.x = (lat_size/2) - changeV.x
         changeV.y = (lat_size/2) - changeV.y
-        points.push(createVector(changeV.x, changeV.y))
-        changeV.reflect(v2)
-        points.push(createVector(changeV.x, changeV.y))
-        changeV.reflect(v1)
-        points.push(createVector(changeV.x, changeV.y))
-        changeV.reflect(v2)
-        points.push(createVector(changeV.x, changeV.y))
-
-        changeV2.reflect(v2)
-        points.push(createVector(changeV2.x, changeV2.y))
-        changeV2.reflect(v1)
-        points.push(createVector(changeV2.x, changeV2.y))
-        changeV2.reflect(v2)
-        points.push(createVector(changeV2.x, changeV2.y))
+        pushIn(points, changeV)
+        quads(v1, v2, points, changeV)
+        quads(v1, v2, points, changeV2)
     }
     else if (pattern == 9) {
         //p4
         changeV.reflect(v2)
         changeV.reflect(v1)
-        points.push(createVector(changeV.x, changeV.y))
+        pushIn(points, changeV)
 
         changeV2.reflect(v2)
         //coord swap for rot
-        points.push(createVector(changeV2.y, changeV2.x))
+        points.push(createVector(changeV.y, changeV.x))
         changeV2.reflect(v2)
         changeV2.reflect(v1)
-        points.push(createVector(changeV2.y, changeV2.x))
+        points.push(createVector(changeV.y, changeV.x))
     }
     else if (pattern == 10) {
         //p4m
         changeV.reflect(v1.add(v2))
-        points.push(createVector(changeV.x, changeV.y))
+        pushIn(points, changeV)
 
-        changeV.reflect(v2)
-        points.push(createVector(changeV.x, changeV.y))
-        changeV.reflect(v1)
-        points.push(createVector(changeV.x, changeV.y))
-        changeV.reflect(v2)
-        points.push(createVector(changeV.x, changeV.y))
-
-        changeV2.reflect(v2)
-        points.push(createVector(changeV2.x, changeV2.y))
-        changeV2.reflect(v1)
-        points.push(createVector(changeV2.x, changeV2.y))
-        changeV2.reflect(v2)
-        points.push(createVector(changeV2.x, changeV2.y))        
+        quads(v1, v2, points, changeV)
+        quads(v1, v2, points, changeV2)         
     }
     else if (pattern == 11) {
         //p4g
+        rots(changeV, changeV2, points, lat_size/2)
+        //rot coord swap
+        let throwaway = changeV.x
+        changeV.x = changeV.y
+        changeV.y = throwaway
+        throwaway = changeV2.x
+        changeV2.x = changeV2.y
+        changeV2.y = throwaway
+        //movement
+        changeV.x -= lat_size/2
+        changeV2.x -= lat_size/2
+        //finish rot
+        pushIn(points, changeV2)
+        rots(changeV, changeV2, points, lat_size/2)
+
+        //reflec
+    }
+    else if (pattern == 12) {
+        
     }
     
     return points
@@ -144,8 +136,46 @@ function patterned(vec) {
 
 //strangely outsourcing not working :(
 //first outsource. life = easier.
-// function quad(v1, v2, points, changeV) {
-// }
+function quads(v1, v2, points, changeV) {
+    changeV.reflect(v2)
+    pushIn(points, changeV)
+    changeV.reflect(v1)
+    pushIn(points, changeV)
+    changeV.reflect(v2)
+    pushIn(points, changeV)
+}
+
+function rots(changeV, changeV2, points, diff) {
+    moveQuad(changeV2, points)
+
+    changeV.x = diff - changeV.x
+    pushIn(points, changeV)
+
+    moveQuad(changeV, points)
+
+    changeV.y = diff - changeV.y
+    pushIn(points, changeV)
+
+    moveQuad(changeV, points) 
+
+    changeV2.y = diff - changeV2.y
+    pushIn(points, changeV2)
+
+    moveQuad(changeV2, points)
+}
+
+function moveQuad(changeV, points) {
+    changeV.x += lat_size/2
+    changeV.y -= lat_size/2
+    pushIn(points, changeV)
+    changeV.x -= lat_size/2
+    changeV.y += lat_size/2
+
+}
+
+function pushIn(points, changeV) {
+    points.push(createVector(changeV.x, changeV.y))
+}
 
 
 function duplicate(points) {
